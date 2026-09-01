963a5f9a08621aa9acfb2a6496cf9138cfba3e07
CaptBlistex <zfrck89yps@privaterelay.appleid.com>
2026-09-01 07:49:41 +0100

    Refresh PAT cache for mail triage update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pat-v3';
+const CACHE='pat-v4';
 const ASSETS=['./','index.html','app.js','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
