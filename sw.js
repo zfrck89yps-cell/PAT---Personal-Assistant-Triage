01af87f7b605306871e8cc71c092314d35314c3b
CaptBlistex <zfrck89yps@privaterelay.appleid.com>
2026-09-01 12:55:05 +0100

    Cache WhatsApp intake test page

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pat-v5';
-const ASSETS=['./','index.html','app.js','manifest.json','icon-192.png','icon-512.png'];
+const CACHE='pat-v6';
+const ASSETS=['./','index.html','app.js','manifest.json','wa-test.html','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))) });
